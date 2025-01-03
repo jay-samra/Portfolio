ee69673b7aff22e405277ebe0e7d95eb56d2ccd3
Jagroop <jagroopsingh1936@gmail.com>
2025-01-02 17:57:54 -0800

    added navbar, added custom font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,14 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+	// import 'tailwindcss/t'
 	let { children } = $props();
+
+
+	import '@fontsource-variable/cinzel';
 </script>
 
 <ParaglideJS {i18n}>
 	{@render children()}
 </ParaglideJS>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- Navbar -->
+<section>
+    <div class="navbar bg-base-10">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+          </div>
+          <a href = '/' class="btn btn-ghost text-xl">Contact Me</a>
+        </div>
+        <!-- put hidden back in this  -->
+        <div class="navbar-center justify-center min-w-[500px] lg:flex w-74 h-15 rounded-[50px] border border-solid border-bg-neutral-900" >
+          <ul class="menu menu-horizontal px-1">
+            <li class="text-lg"><a href = 'https://www.linkedin.com/in/jagroop-s-sam18731/'>LinkedIn</a></li>
+            <li><a class="text-lg" href = 'https://github.com/jay-samra'>GitHub</a></li>
+            <li><a class="text-lg" href = 'https://www.linkedin.com/in/jagroop-s-sam18731/'>Resume</a></li>
+          </ul>
+        </div>
+        <!-- Right side of navbar-->
+        <div class="navbar-end">
+        </div>
+      </div>
+
+</section>
+
+<section>
+    <h1> New Section </h1>
+</section>

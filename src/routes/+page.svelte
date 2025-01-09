533fa7641ca649138943445397b362e110a3270d
Jagroop <jagroopsingh1936@gmail.com>
2025-01-09 10:58:29 -0800

    updated favicon, added images to project cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -136,10 +136,10 @@
     <section class="bg-[#FAF9F6] min-h-screen">
       <div class ="bg-green flex flex-col items-center justify-center align-middle h-full content-between gap-10">
         <hr class="w-48 h-1 mx-auto my-4 bg-[#333333] border-0 rounded md:my-10 dark:bg-gray-700">
-        <h1 class="text-7xl">Here are some of my Projects!</h1>
+        <h1 class="text-7xl">Here Are Some Projects That I am Proud Of!</h1>
         
         <!-- Project Cards -->
-        <div class="flex flex-wrap justify-center space-x-5">
+        <div class="flex flex-wrap justify-center space-x-5 space-y-5">
           <!-- Card 1 -->
           <div class="w-full md:w-1/3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
@@ -150,8 +150,9 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Domestic Violence Data Analysis Website</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A Global & National Case Study on Domestic Violence</p>
+                <div class="space-y-10">
                 <button class="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-medium text-neutral-50"><span class="z-10 pr-2">View Project</span><div class="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-neutral-700 transition-[width] group-hover:w-[calc(100%-8px)]"><div class="mr-3.5 flex items-center justify-center"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></div></button>
-
+              </div>
             </div>
         </div>
             <!-- Card 2 -->
@@ -164,8 +165,9 @@
                       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tutoring Company Website</h5>
                   </a>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A fully functional, responsive website for a tutoring startup</p>
+                  <div class="space-y-10">
                   <button class="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-medium text-neutral-50"><span class="z-10 pr-2">View Project</span><div class="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-neutral-700 transition-[width] group-hover:w-[calc(100%-8px)]"><div class="mr-3.5 flex items-center justify-center"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></div></button>
-
+                </div>
               </div>
           </div>
         <!-- Card 3 -->
@@ -199,7 +201,7 @@
       <!-- Card 5 -->
       <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-            <img class="w-full h-56 object-cover rounded-t-lg" src="/favicon.png" alt="" />
+            <img class="w-full h-56 object-cover rounded-t-lg" src="/java.png" alt="" />
         </a>
         <div class="p-5">
             <a href="#">
@@ -214,7 +216,7 @@
     <!-- Card 6 -->
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-          <img class="w-full h-56 object-cover rounded-t-lg" src="/favicon.png" alt="" />
+          <img class="w-full h-56 object-cover rounded-t-lg" src="/clogo.jpg" alt="" />
       </a>
       <div class="p-5">
           <a href="#">

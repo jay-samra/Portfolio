1106c8183838c779f337ac2c5334e21fb5b609c6
Jagroop <jagroopsingh1936@gmail.com>
2025-02-02 18:14:55 -0800

    changed pallete, updated project card, adding navigational link to arrow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,6 +97,8 @@
           </button>
         </a>
         <!-- Arrow -->
+         <button>
+          <a href="#section1">
         <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2">
           <div class="animate-bounce">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"  class="w-10 h-10 text-black" >
@@ -104,6 +106,8 @@
             </svg>
           </div>
         </div>
+      </a>
+      </button>
       </div>
 
 
@@ -115,7 +119,7 @@
     <!-- About Me -->
     <!-- add 'second-section' to section class for gradient -->
      <!-- OR #FAFAFA -->
-    <section class="bg-[#FAFAFA] min-h-screen ">
+    <section id = "section1" class="bg-[#FAF9F6] min-h-screen ">
       <div class="py-8">
       <hr class="w-48 h-1 mx-auto my-4 bg-gray-700 border-0 rounded md:my-10 dark:bg-gray-700">
       </div>
@@ -172,7 +176,7 @@
                   <a href="#">
                       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tutoring Company Website</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A fully functional, responsive website for a tutoring startup</p>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A fully functional and robust website for a tutoring startup built by utilzing the MERN stack</p>
                   <div class="space-y-10">
                     <a href="https://github.com/wesbruh/TutorWebsite" target = "_blank">
                       <button class="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-medium text-neutral-50"><span class="z-10 pr-2">View Project</span><div class="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-neutral-700 transition-[width] group-hover:w-[calc(100%-8px)]"><div class="mr-3.5 flex items-center justify-center"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></div></button>
@@ -189,7 +193,7 @@
           <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NYC Energy Analysis Dashboard</h5>
           </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Analyzing New York City's energy usage</p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Analyzing New York City's energy usage through data visualizations and interactive graphs</p>
           <a href="https://github.com/jay-samra/NYC-Energy-Analysis" target = "_blank">
             <button class="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-medium text-neutral-50"><span class="z-10 pr-2">View Project</span><div class="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-neutral-700 transition-[width] group-hover:w-[calc(100%-8px)]"><div class="mr-3.5 flex items-center justify-center"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></div></button>
           </a>   
@@ -214,14 +218,14 @@
         <!-- Card 5 -->
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
-              <img class="w-full h-56 object-cover rounded-t-lg" src="/favicon.png" alt="" />
+              <img class="w-full h-56 object-cover rounded-t-lg" src="/journal.png" alt="" />
           </a>
           <div class="p-5">
               <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Social Care Worker Client Journal (In Progress)</h5>
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mental Health Tracking Journal</h5>
               </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A personal portfolio website displaying my experience, projects, and skills</p>
-              <a href="https://github.com/jay-samra/PatientJournal" target = "_blank">
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Journaling website which allows users to track their mental health status, set goals, and improve their well-being</p>
+              <a href="https://github.com/jay-samra/HealthTracker" target = "_blank">
                 <button class="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-medium text-neutral-50"><span class="z-10 pr-2">View Project</span><div class="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-neutral-700 transition-[width] group-hover:w-[calc(100%-8px)]"><div class="mr-3.5 flex items-center justify-center"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-50"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></div></button>
               </a>   
           </div>
@@ -265,7 +269,7 @@
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Leetcode: Blind 75</h5>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Neetcode: Blind 75</h5>
                     </a>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Solving Neetcode's Blind 75 practice interview questions in an efficient manner to help student prepare for technical interviews</p>
                     <a href="https://github.com/jay-samra/blind75" target = "_blank">
@@ -284,7 +288,7 @@
     </section>
 
     <!-- Essays/Blogs -->
-    <section class="bg-[#FAFAFA] min-h-screen ">
+    <section class="bg-[#FAF9F6] min-h-screen ">
       <div class="py-8">
       <hr class="w-48 h-1 mx-auto my-4 bg-gray-700 border-0 rounded md:my-10 dark:bg-gray-700">
       </div>

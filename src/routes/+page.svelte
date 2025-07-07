d04e28d0d7a027ebe9c3e17e46942701432ccd79
Jagroop <jagroopsingh1936@gmail.com>
2025-07-06 23:05:44 -0700

    removed carosuel, replaced with cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -134,12 +134,160 @@
 
         </div>  
         <div class="pt-10">
-        <h1 class="text-7xl">My Tech Stack</h1>
+        <h1 class="text-7xl">My Interests</h1>
       </div>
-      <div class="pb-10">
-              <Carousel ></Carousel>
+      
+      
+      <div class="pb-10 flex flex-wrap" >
+             
+<article
+  class="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]"
+>
+  <div class="w-200 rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
+    <time class="block text-xs text-gray-500" datetime="2019-06-29">
+      
+    </time>
+
+    <a href="#">
+      <h3 class="mt-0.5 text-lg font-medium text-gray-900">
+        UI Design
+      </h3>
+    </a>
+
+    <div class="mt-4 flex flex-wrap gap-1">
+
+
+      <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+                 <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+      
+        
+      </span>
+    </div>
+  </div>
+</article>
+
+<article
+  class="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]"
+>
+  <div class="w-200 rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
+    <time class="block text-xs text-gray-500" datetime="2019-06-29">
+      
+    </time>
+
+    <a href="#">
+      <h3 class="mt-0.5 text-lg font-medium text-gray-900">
+        Backend Implementation
+      </h3>
+    </a>
+
+    <div class="mt-4 flex flex-wrap gap-1">
+
+
+      <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+                 <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+      
+        
+      </span>
+    </div>
+  </div>
+</article>
+
+<article
+  class="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]"
+>
+  <div class="w-200 rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
+    <time class="block text-xs text-gray-500" datetime="2019-06-29">
+      
+    </time>
+
+    <a href="#">
+      <h3 class="mt-0.5 text-lg font-medium text-gray-900">
+        Data Analysis
+      </h3>
+    </a>
+
+    <div class="mt-4 flex flex-wrap gap-1">
+
+
+      <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+            <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+                 <span
+        class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+      >
+      
+        
+      </span>
+    </div>
+  </div>
+</article>
+
             </div>
-      </div>
+      
+      
+          </div>
       
     </section>
 
